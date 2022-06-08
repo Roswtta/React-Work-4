51602ec014df0c3eb5d55f45120dc478117a1acb
Rouzh Jaafar <103349444+Roswtta@users.noreply.github.com>
2022-06-08 17:15:24 +1000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import React, {Component} from "react"
+import Conditional from "./Conditional"
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            unreadMessages: ["Uhh"]
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                {
+                    this.state.unreadMessages.length > 0 &&
+                    <h2>You have {this.state.unreadMessages.length} unread messages!</h2>
+                }
+            </div>
+        )
+    }
+}
+
+export default App
